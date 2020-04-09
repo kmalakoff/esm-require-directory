f89c3150067e31cf20febb65538ebaa4e5cb2bbd
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-09 08:24:14 -0700

    Refactor for dual module

--- compatibility/spec/errors.test.js
+++ compatibility/spec/errors.test.js
@@ -1,13 +1,13 @@
 var path = require('path');
 var chai = require('chai');
 
-var requireDirectory = require('../../..');
+var requireDirectory = require('../..');
 
 var assert = chai.assert;
 
 describe('errors', function () {
   it('fail to import an errored module (cjs)', function (done) {
-    var DATA_DIRECTORY = path.join(__dirname, '..', '..', 'data', 'errors');
+    var DATA_DIRECTORY = path.join(__dirname, '..', 'data', 'errors');
 
     // skips .js
     requireDirectory(DATA_DIRECTORY, { extensions: ['.js'], recursive: false }, function (err, results) {
@@ -17,7 +17,7 @@ describe('errors', function () {
   });
 
   it('fail to import an errored module (mjs)', function (done) {
-    var DATA_DIRECTORY = path.join(__dirname, '..', '..', 'data', 'errors');
+    var DATA_DIRECTORY = path.join(__dirname, '..', 'data', 'errors');
 
     // skips .mjs
     requireDirectory(DATA_DIRECTORY, { extensions: ['.mjs'], recursive: false }, function (err, results) {
