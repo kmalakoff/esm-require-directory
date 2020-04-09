ae6c2fd6ca117e9f0e3a32f2c91f8c892660deb8
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-09 07:43:48 -0700

    Added compatibility testing for previous versions of Node.js

--- index.js
+++ index.js
@@ -1,81 +1,30 @@
-const path = require('path');
-const fs = require('fs');
-const pify = require('pify');
-const walk = require('walk-filtered');
+var walk = require('./lib/walk');
 
-const pStat = pify(fs.lstat);
-const DEFAULT_OPTIONS = {
-  recursive: false,
-  paths: false,
-  default: true,
-  extensions: ['.js'],
-};
+module.exports = function requireDirectory(directory, options, callback) {
+  /* eslint-disable */
+  if (arguments.length === 2 && typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
 
-function fileName(relativePath) {
-  return path.basename(relativePath, path.extname(relativePath));
-}
+  // choose between promise and callback API
+  if (typeof callback === 'function') {
+    options = options || {};
+    options = {
+      recursive: options.recursive,
+      paths: options.paths,
+      filename: options.filename,
+      default: options.default === undefined ? true : options.default,
+      extensions: options.extensions || ['.js'],
+    };
+    if (options.paths && options.filename === undefined) options.filename = true;
 
-function fileDirName(relativePath) {
-  const dirname = path.dirname(relativePath);
-  const filename = fileName(relativePath);
-  return path.join(dirname, filename);
-}
-
-function setResult(results, key, module) {
-  if (results[key] !== undefined) {
-    const value = Array.isArray(results[key]) ? results[key] : [results[key]];
-    results[key] = value.concat(module); // eslint-disable-line no-param-reassign
-  } else results[key] = module;
-}
-
-async function importFile(directory, relativePath, options, results) {
-  if (path.extname(relativePath) === '.mjs') return;
-
-  const module = require(path.join(directory, relativePath));
-
-  // collect result
-  if (options.paths) setResult(results, options.filename ? fileDirName(relativePath) : relativePath, module);
-  else if (options.filename) setResult(results, fileName(relativePath), module);
-  else results.push(module); // eslint-disable-line no-param-reassign
-}
-
-module.exports = async (directory, options = {}) => {
-  options = Object.assign({}, DEFAULT_OPTIONS, options); // eslint-disable-line no-param-reassign
-  if (options.paths && options.filename === undefined) options.filename = true;
-  const results = options.paths || options.filename ? {} : [];
-
-  await walk(
-    directory,
-    async (entry) => {
-      if (entry.path === '') return true;
-
-      // check for index file one level under the directory
-      if (entry.stats.isDirectory()) {
-        if (!options.recursive) return true; // will pick up in traverse
-
-        // check for index file one level under the directory
-        for (const extension of options.extensions) {
-          const relativeIndexPath = path.join(entry.path, `index${extension}`);
-
-          // check for index file one directly down
-          try {
-            const indexStats = await pStat(entry.fullPath);
-            if (indexStats && !indexStats.isDirectory()) {
-              await importFile(directory, relativeIndexPath, options, results);
-              return false; // found index so do not traverse further
-            }
-          } catch (err) {
-            /* no index */
-          }
-        }
-      } else {
-        const extension = path.extname(entry.path);
-        if (~options.extensions.indexOf(extension)) await importFile(directory, entry.path, options, results);
-        return true; // traverse
-      }
-    },
-    { stats: true, depth: options.recursive ? Infinity : 0 }
-  );
-
-  return results;
+    walk(directory, options, callback);
+  } else {
+    return new Promise(function (resolve, reject) {
+      requireDirectory(directory, options, function (err, results) {
+        err ? reject(err) : resolve(results);
+      });
+    });
+  }
 };
