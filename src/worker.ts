0cb30959b17ece8833664a51597fa29cb68eba52
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 17:09:44 -0800

    Upgrade for typescript

---
+++ src/worker.ts
@@ -0,0 +1,12 @@
+import walk from './lib/walk.js';
+
+export default function worker(directory, options_, settings, callback) {
+  const options = { ...options_, ...settings };
+  options.default = options_.default === undefined ? true : options_.default;
+  options.extensions.map((extension) => {
+    if (!~options.extensions.indexOf(extension)) throw new Error(`Extension not supported: ${extension}`);
+  });
+  if (options.paths && options.filename === undefined) options.filename = true;
+
+  walk(directory, options, callback);
+}
