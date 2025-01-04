e1c816a6c4910b304250a556e170221aa8760001
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 20:49:21 -0800

    Refactor imports and update test environment configuration

--- src/index.esm.ts
+++ src/index.esm.ts
@@ -1,11 +1,11 @@
-import worker from './worker.js';
+import worker from './worker';
 
 // @ts-ignore
-import loader from './esm/importFile.js';
+import loader from './esm/importFile';
 const settings = { extensions: ['.mjs'], loader };
 
-import type { RequireCallback, RequireOptions, RequireResult } from './types.js';
-export * from './types.js';
+import type { RequireCallback, RequireOptions, RequireResult } from './types';
+export type * from './types';
 export default function importDirectory(directory: string, options?: RequireOptions | RequireCallback, callback?: RequireCallback): undefined | Promise<RequireResult> {
   if (typeof options === 'function') {
     callback = options;
@@ -13,11 +13,6 @@ export default function importDirectory(directory: string, options?: RequireOpti
   }
   options = options || {};
 
-  // choose between promise and callback API
   if (typeof callback === 'function') return worker(directory, options, settings, callback) as undefined;
-  return new Promise((resolve, reject) => {
-    worker(directory, options, settings, (err, results) => {
-      err ? reject(err) : resolve(results);
-    });
-  });
+  return new Promise((resolve, reject) => worker(directory, options, settings, (err, results) => (err ? reject(err) : resolve(results))));
 }
