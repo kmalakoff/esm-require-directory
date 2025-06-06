11d9922dbdad513be509ab125be2d457c5bc407a
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-05 21:34:56 -0700

    Comprehensive types

--- src/lib/addResult.ts
+++ src/lib/addResult.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import fileName from './fileName.js';
 import filePath from './filePath.js';
 
+import type { Module, RequireEntry, RequireOptions, RequireResult } from '../types.js';
+
 function setResult(results, key, module) {
   if (results[key] !== undefined) {
     const value = Array.isArray(results[key]) ? results[key] : [results[key]];
@@ -9,17 +11,17 @@ function setResult(results, key, module) {
   } else results[key] = module;
 }
 
-export default function addResult(results, entry, options, module) {
+export default function addResult(results: RequireResult, entry: RequireEntry, options: RequireOptions, module: Module) {
   // esm module so extract default by default unless asked not to
   if (path.extname(entry.basename) === '.mjs') {
     if (options.default === undefined || options.default) {
       // check default
       if (module.default === undefined) return; // no default
-      module = module.default;
+      module = module.default as Module;
     }
   }
 
   if (options.paths) setResult(results, options.filename ? filePath(entry.path) : entry.path, module);
   else if (options.filename) setResult(results, fileName(entry.path), module);
-  else results.push(module);
+  else (results as unknown[]).push(module);
 }
