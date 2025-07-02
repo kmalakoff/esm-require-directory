6a711694f2730d126fe2d9cde3a4c31998a7be3d
Kevin Malakoff <kmalakoff@gmail.com>
2025-07-02 06:59:15 -0700

    Upgrade modules

--- src/lib/addResult.ts
+++ src/lib/addResult.ts
@@ -1,18 +1,20 @@
-import path from 'path';
 import type { Module, RequireEntry, RequireOptions, RequireResult } from '../types.ts';
 import fileName from './fileName.ts';
 import filePath from './filePath.ts';
 
+const isArray = Array.isArray || ((x) => Object.prototype.toString.call(x) === '[object Array]');
+const endsWith = (string, check) => string.indexOf(check, string.length - check.length) !== -1;
+
 function setResult(results, key, module) {
   if (results[key] !== undefined) {
-    const value = Array.isArray(results[key]) ? results[key] : [results[key]];
+    const value = isArray(results[key]) ? results[key] : [results[key]];
     results[key] = value.concat(module);
   } else results[key] = module;
 }
 
 export default function addResult(results: RequireResult, entry: RequireEntry, options: RequireOptions, module: Module) {
   // esm module so extract default by default unless asked not to
-  if (path.extname(entry.basename) === '.mjs') {
+  if (endsWith(entry.basename, '.mjs')) {
     if (options.default === undefined || options.default) {
       // check default
       if (module.default === undefined) return; // no default
