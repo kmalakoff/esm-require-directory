131cf432338c81011ae5def758e936d9016ae6c1
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-20 12:34:09 -0800

    Migrated to void and simplified compat layer

--- src/lib/addResult.ts
+++ src/lib/addResult.ts
@@ -1,9 +1,9 @@
+import { stringEndsWith } from '../compat.ts';
 import type { Module, RequireEntry, RequireOptions, RequireResult } from '../types.ts';
 import fileName from './fileName.ts';
 import filePath from './filePath.ts';
 
 const isArray = Array.isArray || ((x) => Object.prototype.toString.call(x) === '[object Array]');
-const endsWith = (string, check) => string.indexOf(check, string.length - check.length) !== -1;
 
 function setResult(results, key, module) {
   if (results[key] !== undefined) {
@@ -14,7 +14,7 @@ function setResult(results, key, module) {
 
 export default function addResult(results: RequireResult, entry: RequireEntry, options: RequireOptions, module: Module) {
   // esm module so extract default by default unless asked not to
-  if (endsWith(entry.basename, '.mjs')) {
+  if (stringEndsWith(entry.basename, '.mjs')) {
     if (options.default === undefined || options.default) {
       // check default
       if (module.default === undefined) return; // no default
