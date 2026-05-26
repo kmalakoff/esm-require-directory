c1ebc727a220c13f2446ff24a4cb76803befc7d7
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 14:23:41 -0700

    Fix broken types #2

--- src/cjs/requireFile.ts
+++ src/cjs/requireFile.ts
@@ -1,12 +1,12 @@
 import Module from 'module';
 
 const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
-export type Callback = (error?: Error, module?: unknown) => void;
+export type Callback = (error?: Error | null, module?: unknown) => void;
 
 export default function requireFile(fullPath: string, callback: Callback): void {
   try {
     callback(undefined, _require(fullPath));
   } catch (err) {
-    callback(err as Error | undefined);
+    callback(err as Error | null);
   }
 }
